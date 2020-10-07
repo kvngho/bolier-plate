29bc611b4a90b71b948bffdf60ea1701150eed81
kangho_lab <kangho@hanyang.ac.kr>
2020-10-07 17:25:55 +0900

    비밀번호 암호화

--- models/User.js
+++ models/User.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const saltRounds = 10; // salt 를 이용하여 비밀번호 암호화
 const userSchema = mongoose.Schema({
   name: {
     type: String,
@@ -29,6 +31,21 @@ const userSchema = mongoose.Schema({
     type: Number,
   },
 });
+
+userSchema.pre("save", function (next) {
+  var user = this;
+  //비밀번호를 암호화 시킨다.
+  if (user.isModified("password")) {
+    bcrypt.genSalt(saltRounds, function (err, salt) {
+      if (err) return next(err);
+      bcrypt.hash(user.password, salt, function (err, hash) {
+        if (err) return next(err);
+        user.password = hash;
+        next();
+      });
+    });
+  }
+}); // 모델에 저장하기 전에 무엇을 다다
 // 위에 만든 스키마를 모델로 감싸준다
 const User = mongoose.model("User", userSchema);
 
