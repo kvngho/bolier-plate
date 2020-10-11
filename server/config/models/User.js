7f63213cb2e257eba78c4881918da113e9eb9231
이강호 <kangho@iganghoui-Macmini.local>
2020-10-11 13:11:42 +0900

    20201011

--- server/config/models/User.js
+++ server/config/models/User.js
@@ -1,5 +1,7 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
 const saltRounds = 10; // salt 를 이용하여 비밀번호 암호화
 const userSchema = mongoose.Schema({
   name: {
@@ -33,8 +35,8 @@ const userSchema = mongoose.Schema({
 });
 
 userSchema.pre("save", function (next) {
-  var user = this;
-  //비밀번호를 암호화 시킨다.
+  const user = this;
+  // 비밀번호를 암호화 시킨다.
   if (user.isModified("password")) {
     bcrypt.genSalt(saltRounds, function (err, salt) {
       if (err) return next(err);
@@ -44,9 +46,42 @@ userSchema.pre("save", function (next) {
         next();
       });
     });
+  } else {
+    next();
   }
 }); // 모델에 저장하기 전에 무엇을 다다
 // 위에 만든 스키마를 모델로 감싸준다
+
+userSchema.methods.comparePassword = function (plainPassword, cb) {
+  bcrypt.compare(plainPassword, this.password, function (err, isMatch) {
+    if (err) {
+      return cb(err);
+    }
+    cb(null, isMatch);
+  });
+};
+userSchema.methods.generateToken = function (cb) {
+  const user = this;
+  const token = jwt.sign(user._id.toHexString(), "secretToken");
+  user.token = token;
+  user.save(function (err, user) {
+    if (err) return cb(err);
+    cb(null, user);
+  });
+  // 토큰을 생성하기
+};
+userSchema.statics.findByToken = function (token, cb) {
+  const user = this;
+  jwt.verify(token, "secretToken", function (err, decoded) {
+    user.findOne({ _id: decoded, token }, function (err, user) {
+      if (err) return cb(err);
+      cb(null, user);
+    });
+    // 유저아이디를 이용해서 유저를 찾은 다음에
+    // 클라이언트에서 가져온 토큰과 디비에 보관된 토큰이 일치하는지 확인
+  });
+};
+
 const User = mongoose.model("User", userSchema);
 
 module.exports = { User };
